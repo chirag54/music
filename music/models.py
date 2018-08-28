f547893fe3f65cd680d3340e13cdf8300031c414
chirag54 <chauhanchirag662@gmail.com>
2018-08-28 12:21:45 +0530

    music  file update

--- music/models.py
+++ music/models.py
@@ -17,14 +17,23 @@ class Album(models.Model):
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.id})
 
-    def _str_(self):
-        return self.album_title + '-' + self.artist
+    
+    def __str__(self):
+        return "%s" % (self.album_title)
+
 
 
 class Song(models.Model):
-    album = models.ForeignKey(Album)
+    model = Album
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.file_type
+
+    class Meta:
+        ordering = ('file_type',)
 
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.album.id})
